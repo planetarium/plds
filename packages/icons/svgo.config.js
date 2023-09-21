be733ba02b17e0ccea8a163ec8dec38324ab894e
Kimbug <kimbug@planetariumhq.com>
2023-09-21 09:37:56 +0900

    feat(setup): install svgo to generate icon library

---
+++ packages/icons/svgo.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  multipass: true,
+  js2svg: {
+    indent: 2,
+    pretty: true,
+  },
+  plugins: [
+    { name: 'preset-default' },
+    'sortAttrs',
+    'removeScriptElement',
+    'removeDimensions',
+    'removeScriptElement',
+    'removeDoctype',
+    'removeEmptyText',
+  ],
+}
