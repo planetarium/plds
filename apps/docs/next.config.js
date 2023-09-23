08cedd9c57bd76d95b348cdd5d0cdf88aa758435
Kimbug <kimbug@planetariumhq.com>
2023-09-23 09:58:25 +0900

    chore(assets): upload svg assets

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["@plds/ui"],
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
+  transpilePackages: ['@plds/ui'],
+}
---
+++ packages/icons/dist/cjs/index.js
@@ -0,0 +1,4 @@
+module.exports.PlanetariumHorizontalIcon = require('./PlanetariumHorizontalIcon.js');
+module.exports.PlanetariumSymbolIcon = require('./PlanetariumSymbolIcon.js');
+module.exports.PlanetariumVerticalIcon = require('./PlanetariumVerticalIcon.js');
+module.exports.PlanetariumWordmarkIcon = require('./PlanetariumWordmarkIcon.js');
