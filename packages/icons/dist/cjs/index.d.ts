de945d50e82e33364e19a81c2ba779153665bb3e
Kimbug <kimbug@planetariumhq.com>
2023-09-23 09:58:25 +0900

    chore(assets): upload svg assets

---
+++ packages/icons/dist/cjs/index.d.ts
@@ -0,0 +1,4 @@
+export { default as PlanetariumHorizontalIcon } from './PlanetariumHorizontalIcon';
+export { default as PlanetariumSymbolIcon } from './PlanetariumSymbolIcon';
+export { default as PlanetariumVerticalIcon } from './PlanetariumVerticalIcon';
+export { default as PlanetariumWordmarkIcon } from './PlanetariumWordmarkIcon';
---
+++ packages/icons/dist/esm/index.js
@@ -0,0 +1,4 @@
+export { default as PlanetariumHorizontalIcon } from './PlanetariumHorizontalIcon.js';
+export { default as PlanetariumSymbolIcon } from './PlanetariumSymbolIcon.js';
+export { default as PlanetariumVerticalIcon } from './PlanetariumVerticalIcon.js';
+export { default as PlanetariumWordmarkIcon } from './PlanetariumWordmarkIcon.js';
