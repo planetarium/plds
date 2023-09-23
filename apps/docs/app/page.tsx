e839b6e2b9617976c763675bf5fd2b0c7fa43607
Kimbug <kimbug@planetariumhq.com>
2023-09-23 09:58:25 +0900

    chore(assets): upload svg assets

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,20 @@
 import { Button, Header } from '@plds/ui'
+import {
+  PlanetariumHorizontalIcon,
+  PlanetariumSymbolIcon,
+  PlanetariumVerticalIcon,
+  PlanetariumWordmarkIcon,
+} from '@plds/icons'
 
 export default function Page() {
   return (
     <>
       <Header text="Docs" />
       <Button />
+      <PlanetariumHorizontalIcon />
+      <PlanetariumSymbolIcon />
+      <PlanetariumVerticalIcon />
+      <PlanetariumWordmarkIcon />
     </>
   )
 }
---
+++ packages/icons/dist/esm/PlanetariumSymbolIcon.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare function PlanetariumSymbolIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element;
+export default PlanetariumSymbolIcon;
