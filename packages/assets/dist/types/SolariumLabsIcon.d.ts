0f904bf99ece14eeb2649c0a3f10585e3fa34acc
Kimbug <kimbug@planetariumhq.com>
2023-09-29 15:35:55 +0900

    refactor(rename): move planetarium/**/* to brand/

---
+++ packages/assets/dist/types/SolariumLabsIcon.d.ts
@@ -0,0 +1,7 @@
+
+      import * as React from 'react';
+      declare function SolariumLabsIcon(props: React.SVGProps<SVGSVGElement> & {
+        type?: 'horizontal' | 'symbol' | 'vertical' | 'wordmark' 
+      }): JSX.Element;
+      export default SolariumLabsIcon;
+    
