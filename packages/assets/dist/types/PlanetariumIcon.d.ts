e285df301fc8dedcf4f6f90353b24d613f397747
Kimbug <kimbug@planetariumhq.com>
2023-10-04 15:17:04 +0900

    feat(assets): add solid brand assets

--- packages/assets/dist/types/PlanetariumIcon.d.ts
+++ packages/assets/dist/types/PlanetariumIcon.d.ts
@@ -2,6 +2,8 @@
       import * as React from 'react';
       declare function PlanetariumIcon(props: React.SVGProps<SVGSVGElement> & {
         type?: 'horizontal' | 'symbol' | 'vertical' | 'wordmark' 
+        solid?: booleand
+        theme?: 'light' | 'dark'
       }): JSX.Element;
       export default PlanetariumIcon;
     
