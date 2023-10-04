23baec7e0175335e194bc7e0b1b2001558ac84aa
Kimbug <kimbug@planetariumhq.com>
2023-10-04 15:17:04 +0900

    feat(assets): add solid brand assets

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,20 +1,61 @@
-import { Button, Header } from '@plds/ui'
+import { Box, Flex } from '@chakra-ui/react'
 import {
-  PlanetariumHorizontalIcon,
-  PlanetariumSymbolIcon,
-  PlanetariumVerticalIcon,
-  PlanetariumWordmarkIcon,
-} from '@plds/icons'
+  DiscordIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  GoogleIcon,
+  NineCorporationIcon,
+  RedditIcon,
+  SnapchatIcon,
+  SolariumLabsIcon,
+} from '@plds/assets'
+import { InfoIcon } from '@plds/icons'
+import { Button, Header } from '@plds/ui'
 
 export default function Page() {
   return (
-    <>
+    <Box color="primary.500">
       <Header text="Docs" />
       <Button />
-      <PlanetariumHorizontalIcon />
-      <PlanetariumSymbolIcon />
-      <PlanetariumVerticalIcon />
-      <PlanetariumWordmarkIcon />
-    </>
+      <InfoIcon size={12} />
+      <InfoIcon size={16} />
+      <InfoIcon size={24} />
+      <InfoIcon size={32} />
+      <InfoIcon size={40} />
+
+      <GoogleIcon />
+      <RedditIcon solid circle />
+      <FacebookMessengerIcon />
+      <FacebookIcon circle />
+      <SnapchatIcon circle />
+      <DiscordIcon circle />
+
+      <h2>Solarium labs</h2>
+      <Flex>
+        <SolariumLabsIcon solid style={{ width: 100, height: 'auto' }} />
+        <SolariumLabsIcon
+          solid
+          style={{ width: 100, height: 'auto' }}
+          type="symbol"
+        />
+        <SolariumLabsIcon
+          style={{ width: 100, height: 'auto' }}
+          type="vertical"
+        />
+      </Flex>
+      <hr style={{ margin: '100px 0' }} />
+      <h2>Nine chronicles</h2>
+      <Flex>
+        <NineCorporationIcon style={{ width: 100, height: 'auto' }} />
+        <NineCorporationIcon
+          style={{ width: 100, height: 'auto' }}
+          type="symbol"
+        />
+        <NineCorporationIcon
+          style={{ width: 100, height: 'auto' }}
+          type="vertical"
+        />
+      </Flex>
+    </Box>
   )
 }
--- packages/assets/dist/types/PlanetariumLabsIcon.d.ts
+++ packages/assets/dist/types/PlanetariumLabsIcon.d.ts
@@ -2,6 +2,8 @@
       import * as React from 'react';
       declare function PlanetariumLabsIcon(props: React.SVGProps<SVGSVGElement> & {
         type?: 'horizontal' | 'symbol' | 'vertical' | 'wordmark' | 'multiline'
+        solid?: booleand
+        theme?: 'light' | 'dark'
       }): JSX.Element;
       export default PlanetariumLabsIcon;
     
