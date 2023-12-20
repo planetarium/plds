5b6b67f4979972152924b21df96a0d9964cd4eff
Kimbug <kimbug@planetariumhq.com>
2023-12-20 10:47:43 +0900

    feat: export badge components and add README.md

---
+++ packages/ui/badge/index.tsx
@@ -0,0 +1,4 @@
+export { Badge } from './Badge'
+export type { BadgeProps } from './Badge'
+export { BadgeDot } from './BadgeDot'
+export type { BadgeDotProps } from './BadgeDot'
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,5 +1,6 @@
 export { default as Divider } from './divider'
 export { default as PldsProvider } from './provider'
+export * from './badge'
 export * from './button'
 export * from './skeleton'
 export * from './tooltip'
