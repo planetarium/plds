70c1b0ea14a68b88ff7210a1c4888c2498b70bc6
Akamig <akamig@posteo.net>
2023-12-20 16:49:06 +0900

    fix: remove "CurrentColor" and add in index

--- packages/theme/components/index.ts
+++ packages/theme/components/index.ts
@@ -1,11 +1,13 @@
 import { default as Button } from './button'
 import { default as Input } from './input'
 import { default as Radio } from './radio'
+import { default as Checkbox } from './checkbox'
 
 const components = {
   Button,
   Input,
-  Radio
+  Radio,
+  Checkbox
 }
 
 export default components
